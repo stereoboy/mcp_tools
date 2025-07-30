10db8fbd2fb59e4dba94070eab16f08b91028710
Jaeyoung Lee <jylee0121@gmail.com>
2025-07-30 19:58:50 +0900

    feat: add message response capability for Slack channel
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- slack-app/app.js
+++ slack-app/app.js
@@ -14,12 +14,25 @@ const app = new App({
 // Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
+  console.log('Received message:', message);
+  console.log('Message channel type:', message.channel_type);
   await say(`Hey there <@${message.user}>!`);
+});
+
+// Listen to ALL direct messages for debugging
+app.message(async ({ message, say }) => {
+  // Only log DMs to avoid spam
+  if (message.channel_type === 'im') {
+    console.log('DM received:', message.text);
+    console.log('From user:', message.user);
+  }
+  console.log(message);
 });
 
 // Listen for a slash command invocation
 app.command('/echo', async ({ command, ack, respond }) => {
   // Acknowledge the command request
+  console.log(command);
   await ack();
 
   await respond(`${command.text}`);
